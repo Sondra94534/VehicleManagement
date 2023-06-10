04fa571f8db433394f4a82a70962b69b7d006824
Sondra Hoffman <104171381+Sondra94534@users.noreply.github.com>
2023-06-10 13:51:29 -0700

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,61 +1,54 @@
-package application;
+/*
+Created by Sondra Hoffman 
+Date: 12/01/2019
+CSC372 - Programming II
+Colorado State University - Global Campus
 
-import java.util.LinkedList;
+Purpose:The Vehicle class represents a basic vehicle with attributes such as make, model, and miles per gallon (mpg). 
+
+*/
+
+package application;
 
 public class Vehicle {
 	
 	private String make;
 	private String model;
-	private double mpg;//Field for miles per gallon.
+	private double mpg;
 	
-	public Vehicle (String make, String model, double mpg) {
-		super();
+	public Vehicle(String make, String model, double mpg) {
 		this.make = make;
 		this.model = model;
 		this.mpg = mpg;
 	}
 	
-	/**
-	 * @return the make
-	 */
 	public String getMake() {
 		return make;
 	}
-	/**
-	 * @param make the make to set
-	 */
+	
 	public void setMake(String make) {
 		this.make = make;
 	}
-	/**
-	 * @return the model
-	 */
+	
 	public String getModel() {
 		return model;
 	}
-	/**
-	 * @param model the model to set
-	 */
+	
 	public void setModel(String model) {
 		this.model = model;
 	}
-	/**
-	 * @return the miles per gallon
-	 */
+	
 	public double getMpg() {
 		return mpg;
 	}
-	/**
-	 * @param mpg the miles per gallon to set
-	 */
+	
 	public void setMpg(double mpg) {
 		this.mpg = mpg;
 	}
 
 	@Override
 	public String toString() {
-		return "Vehicle [make=" + make + ", model=" + model + ", mpg=" + mpg + "]"+System.lineSeparator();
+		return "Vehicle [make=" + make + ", model=" + model + ", mpg=" + mpg + "]";
 	}
-	
-
 }
+
