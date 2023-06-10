467116d7722ae01388f615a664146cb7c80f2d0c
Sondra Hoffman <104171381+Sondra94534@users.noreply.github.com>
2023-06-10 13:54:20 -0700

    Update SortVehicles.java

--- SortVehicles.java
+++ SortVehicles.java
@@ -1,17 +1,26 @@
-package application;
+/*
+Created by Sondra Hoffman
+Date: 12/01/2019
+CSC372 - Programming II 
+Colorado State University - Global Campus
+
+Purpose: 
+The code uses a custom implementation of the Comparator interface in Java. 
+It defines a compare method that takes two Vehicle objects as arguments and compares them based on their miles per gallon (mpg) values.
+*/
 
 import java.util.Comparator;
 
-public class SortVehicles implements Comparator<Vehicle>{
+public class SortVehicles implements Comparator<Vehicle> {
 
-	public int compare(Vehicle v1, Vehicle v2) {
-		if (v1.getMpg()>v2.getMpg())
-			
-			return 1; 
-		
-		else 
-			
-			return -1;
-	}
-
+    @Override
+    public int compare(Vehicle v1, Vehicle v2) {
+        if (v1.getMpg() > v2.getMpg())
+            return 1;
+        else if (v1.getMpg() < v2.getMpg())
+            return -1;
+        else
+            return 0;
+    }
 }
+
